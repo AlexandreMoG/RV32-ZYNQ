7096bb436113e754fd336a6e5f02f791eadd565e
MoralAlexandre <alexandre.moral@etudiant.univ-perp.fr>
2024-02-12 16:02:06 +0100

    Maj du Journal de bord et fichiers lib

---
+++ Program/GPIO_Lib/gpio.c
@@ -0,0 +1,61 @@
+#include "gpio.h"
+
+uint32_t read_register(uint32_t address)
+{
+    uint32_t reg;
+    asm(    "lw %0, 0(%1);"
+            :"=r;"(reg)
+            :"r"(address)
+    );
+    return reg;
+}
+
+void write_register(uint32_t address,uint32_t data)
+{
+    uint32_t reg;
+    asm(    "sw %0, 0(%1);"
+            :
+            : "=r;"(data), "=r"(address)
+    );
+}
+
+
+void config_led(int i)
+{
+    uint32_t tri_state_led;
+    if (i>31) return;
+    //Read register
+    tri_state_led = read_register(GPIO_SWS_LEDS+GPIO_2TRI_OFFSET);
+    tri_state_led = REP_BITS(tri_state_led,i,1,OUTPUT);
+    //Write to register
+    write_register(GPIO_SWS_LEDS+GPIO_2TRI_OFFSET,tri_state_led);
+}
+
+void config_switch(int i)
+{
+    uint32_t tri_state_sws;
+    if (i>31) return;
+    //Read register
+    tri_state_sws = read_register(GPIO_SWS_LEDS+GPIO_TRI_OFFSET);
+    tri_state_sws = REP_BITS(tri_state_sws,i,1,INPUT);
+    //Write to register
+    write_register(GPIO_SWS_LEDS+GPIO_2TRI_OFFSET,tri_state_sws);
+}
+
+int read_switch(int i)
+{
+    uint32_t tri_data_sws;
+    if (i>31) return -1;
+    //Read register
+    tri_data_sws = read_register(GPIO_SWS_LEDS+GPIO_DATA_OFFSET);
+    tri_data_sws = GET_BITS(tri_data_sws,i,1);
+    //Write to register
+    return tri_data_sws;
+}
+
+void set_led(int i)
+{
+    if (i>31) return;
+    uint32_t data = (1 << i);
+    write_register(GPIO_SWS_LEDS+GPIO_2TRI_OFFSET,data);
+}
---
+++ Program/GPIO_Lib/gpio.h
@@ -0,0 +1,25 @@
+#include <stdint.h>
+
+#define GPIO_DATA_OFFSET     0x0000
+#define GPIO_TRI_OFFSET      0x0004
+#define GPIO_2DATA_OFFSET    0x0008
+#define GPIO_2TRI_OFFSET     0x000c
+#define GIER_OFFSET          0x011c
+#define IP_IER_OFFSET        0x0128
+#define IP_ISR_OFFSET        0x0120
+
+#define GPIO_BUTTONS         0x40000000
+#define GPIO_SWS_LEDS        0x40010000
+
+//Macro
+#define MASK(l)	((1 << (l))-1)
+#define GET_BITS(x, i, l)		(((x)>>(i))&MASK(l))
+#define REP_BITS(x, i, l, y)	(((x)&~(MASK(l)<<i))|((y)<<(i)))
+
+//Output / input
+#define OUTPUT 0
+#define INPUT 1
+
+
+uint32_t read_register(uint32_t address);
+uint32_t write_register(uint32_t address,uint32_t data);
